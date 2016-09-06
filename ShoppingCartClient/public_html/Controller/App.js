4062a463241a91864cfc60265d74afea1d897808
AbbasZahid <abbaszahid1990@gmail.com>
2016-09-06 20:34:38 -0300

    Code Improvment
    js function is fixed.

--- ShoppingCartClient/public_html/Controller/App.js
+++ ShoppingCartClient/public_html/Controller/App.js
@@ -1,16 +1,3 @@
-//$(document).ready(function() {
-//    var data = {image : 'image', name : 'name', price : 'price', productId : 'productId'};
-//    $.ajax({
-//        dataType: "json",
-//        url: 'http://localhost:8084/ShoppingCart/rest/service/product',
-//        type: 'GET',
-//        data: data,
-//        success: function() {
-//            alert('PUT completed');
-//        }
-//    });
-//});
-
 var app = angular.module("switchableGrid", ['ngResource']);
 
 function SwitchableGridController($scope, $http){
@@ -48,12 +35,7 @@ function SwitchableGridController($scope, $http){
                     headers: {'Content-type': 'application/json'}
             }).success(function (data) {
                 console.log(data);
-                //TODO: Fix this body here
-//                $scope.items.image = data[0].image;
-//                $scope.items.name = data[0].name;
-//                $scope.items.price = data[0].price;
-//                $scope.items.product_id = data[0].product_id;
-//                $scope.showResult1.push($scope.items);
+                $scope.showResult = data;
             }).error(function () {
                 console.log("Service does not works");
             });
@@ -66,11 +48,6 @@ function SwitchableGridController($scope, $http){
             }).success(function (data) {
                 console.log(data);
                 $scope.items = data;
-//                $scope.items.image = data[0].image;
-//                $scope.items.name = data[0].name;
-//                $scope.items.price = data[0].price;
-//                $scope.items.product_id = data[0].product_id;
-//                $scope.showResult1.push($scope.items);
             }).error(function () {
                 console.log("Service does not works");
             });
